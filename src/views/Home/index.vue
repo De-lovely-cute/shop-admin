519502e97c9829f90b7d10a45ecec0d66fde8369
1431836316@qq.com <17378243469hs>
2023-05-08 21:15:09 +0800

    登录完成

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,19 +1,18 @@
 <script setup>
-import { showModal, notifc } from '@/componsables/util.js'
-import { logout } from '@/api/manger.js'
+import { showModal, notifc } from "@/componsables/util.js";
+import { logout } from "@/api/manger.js";
 import { useStore } from "vuex";
-import { useRouter } from 'vue-router'
-const store = useStore()
-const router = useRouter()
+import { useRouter } from "vue-router";
+const store = useStore();
+const router = useRouter();
 const outlogin = () => {
-    showModal("退出登录成功").then((res) => {
-        logout().then(res => {
-            store.dispatch("logout")
-        router.push('/login')
-        notifc("退出登录成功", "success")
-        })
-        
-    })
+  showModal("退出登录成功").then((res) => {
+    logout().then((res) => {
+      store.dispatch("logout");
+      router.push("/login");
+      notifc("退出登录成功", "success");
+    });
+  });
 };
 </script>
 
